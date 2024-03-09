b93737610618bc9a908447e6536d35618d2693f3
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-03-09 19:56:21 +0530

    Code For First 3 Question

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let obj1 = {name:"Person1",age:5};
+let obj2 = {age:5,name:"Person1"};
+
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
+//Question 2 and 3
 var request = new XMLHttpRequest();
 request.open("GET"," https://restcountries.com/v3.1/all ")
 request.send();

57faeeef0cb77a5e5f79814e6874af0de6450797
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-03-12 22:15:18 +0530

    Final

--- Anonymous.js
+++ Anonymous.js
@@ -65,3 +65,66 @@ var prime=function num(arr)
 }
 console.log(prime([1,2,3,4,5,12,19,99,67]))
 
+//Return all the palindromes in an array
+var palindrome = function words(arr)
+{
+    var ans=[]
+    for(var i=0;i<arr.length;i++)
+    {
+         var res=arr[i].split('').reverse().join('')
+         if(arr[i]===res)
+         {
+           ans.push(arr[i])
+         }
+         
+    }
+    return ans;
+    
+}
+console.log(palindrome(["guvi","mam","racecar"]))
+
+//Q-6 Median of two sorted arrays
+var median=function mid(arr1,arr2){
+  var res = [...arr1,...arr2]
+  var a = (res.length/2)-1;
+  var b = ((res.length/2)+1)-1;
+  var c = res[a]+res[b]
+  var d =c/2
+  return d;
+}
+console.log(median([1,2,3],[4,5,6]))
+
+//Q-7 Remove Duplicates from the array
+var duplicate=function copy(arr){
+  var obj={}
+  var arr1=[]
+  for(var i=0;i<arr.length;i++)
+  {
+    var element=arr[i]
+    if(obj[element])
+    {
+      obj[element]++
+    }
+    else
+    {
+      obj[element]=1
+    }
+    if(obj[element]==1)
+    {
+      arr1.push(element)
+    }
+  }
+  
+  return arr1
+}
+console.log(duplicate([1,2,2,3,4,3,4,6,6,6]))
+
+//Q-8 Rotate an array K times [Left rotation]
+var rotateArray1 = function(nums, k) {
+  for (let i = 0; i < k; i++) {
+      nums.push(nums.shift()); // Change the direction of shift to perform left rotation
+  }
+  return nums;
+}
+
+console.log(rotateArray1([1, 2, 3, 4, 5], 6)); // Output: [3, 4, 5, 1, 2]
